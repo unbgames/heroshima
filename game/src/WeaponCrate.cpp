b2f440b15cf00db8c14f71181b08450c3703ebac
EdgarFabiano <eddy.led@gmail.com>
2018-05-29 00:32:42 -0300

    Adding sound on catching new weapon

--- game/src/WeaponCrate.cpp
+++ game/src/WeaponCrate.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <Player.h>
+#include <Sound.h>
+#include <Game.h>
 #include "WeaponCrate.h"
 
 WeaponCrate::WeaponCrate(GameObject &associated, const Vec2 &initialPosition, Gun* gun, bool startFalling) :
@@ -14,4 +16,12 @@ void WeaponCrate::OnCatch() {
     gun->setAmmo(gun->getFull());
     Player::playerBody->SetGun(gun);
     associated.RequestDelete();
+
+    //TODO garantir que esse GO eh deletado apos terminar de tocar o som
+    auto bulletGO(new GameObject);
+    auto sound(new Sound(*bulletGO, "audio/heavy_machine_gun.wav"));
+    auto recharge(new Sound(*bulletGO, "audio/recarregar.ogg"));
+    sound->Play();
+    recharge->Play();
+
 }
