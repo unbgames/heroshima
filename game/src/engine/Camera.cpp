f0310ed73aacd01a6ce977fc814c4d0b8a64e2ba
Unknown <140083162@aluno.unb.br>
2018-07-07 14:43:25 -0300

    Got back logic of camera control; Added scale and offset to bullet hitbox.

--- game/src/engine/Camera.cpp
+++ game/src/engine/Camera.cpp
@@ -36,6 +36,38 @@ void Camera::Update(float dt) {
                 pos += {0, focus->box.y - BODY_OFFSET};
             }
         }
+    } else {
+        InputManager inputManager = InputManager::GetInstance();
+
+        speed = {0, 0};
+        Vec2 direcional = {CAMERA_SPEED * dt, 0};
+
+        bool right = inputManager.IsKeyDown(RIGHT_ARROW_KEY);
+        bool down = inputManager.IsKeyDown(DOWN_ARROW_KEY);
+        bool left = inputManager.IsKeyDown(LEFT_ARROW_KEY);
+        bool up = inputManager.IsKeyDown(UP_ARROW_KEY);
+
+        if (right) {
+            speed = direcional;
+        } else if (down) {
+            speed = direcional.Rotate((float) M_PI / 2);
+        } else if (left) {
+            speed = direcional.Rotate((float) M_PI);
+        } else if (up) {
+            speed = direcional.Rotate((float) -M_PI / 2);
+        }
+
+        if (right && down) {
+            speed = direcional.Rotate((float) M_PI / 4);
+        } else if (left && down) {
+            speed = direcional.Rotate((float) 3 * M_PI / 4);
+        } else if (left && up) {
+            speed = direcional.Rotate((float) -3 * M_PI / 4);
+        } else if (right && up) {
+            speed = direcional.Rotate((float) -M_PI / 4);
+        }
+
+        pos += speed;
     }
 
     if(wiggle){
