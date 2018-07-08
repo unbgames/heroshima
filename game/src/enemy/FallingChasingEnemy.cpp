886898a7dd6735572522d83e8ef061530119acd3
Unknown <140083162@aluno.unb.br>
2018-07-08 19:47:25 -0300

    Added assassin enemy.

--- game/src/enemy/FallingChasingEnemy.cpp
+++ game/src/enemy/FallingChasingEnemy.cpp
@@ -8,18 +8,20 @@
 #include <Bullet.h>
 #include <Collider.h>
 #include <Player.h>
+#include <Sound.h>
+#include <Game.h>
 #include "FallingChasingEnemy.h"
 
 FallingChasingEnemy::FallingChasingEnemy(GameObject &associated, int hp, Vec2 initialPosition, bool startFalling)
         : Enemy(associated, hp, initialPosition), fell(false), landed(false), hit(false), speed({0, 0}) {
 
-    falling = StaticSprite("img/enemy_falling.png", 2, 0.3f);
-    chasing = StaticSprite("img/enemy_running.png", 12, 0.06f);
-    idle = StaticSprite("img/enemy_idle.png", 6, 0.2f);
-    stopped = StaticSprite("img/enemy_idle.png", 6, 0.2f);
-    preparing = StaticSprite("img/enemy_preparing.png", 4, 0.1f);
-    attacking = StaticSprite("img/enemy_attacking.png", 8, 0.1f);
-    deadByBullet = StaticSprite("img/enemy_dying_gun.png", 20, 0.1f, 2);
+    falling = StaticSprite("img/assassin_idel.png", 2, 0.2f);
+    chasing = StaticSprite("img/assassin_andando.png", 6, 0.06f);
+    idle = StaticSprite("img/assassin_idel.png", 4, 0.2f);
+    stopped = StaticSprite("img/assassin_idel.png", 4, 0.2f);
+    preparing = StaticSprite("img/assassin_idel.png", 4, 0.2f);
+    attacking = StaticSprite("img/assassin_ataque.png", 8, 0.08f);
+    deadByBullet = StaticSprite("img/assassin_deadshot.png", 7, 0.1f, 7);
 
     state = E_STOPPED;
 
@@ -29,15 +31,16 @@ FallingChasingEnemy::FallingChasingEnemy(GameObject &associated, int hp, Vec2 in
         associated.box += {0, -3*associated.box.h};
     }
 
-    associated.orientation = RIGHT;
+    associated.orientation = LEFT;
     associated.AddComponent(img);
 }
 
 void FallingChasingEnemy::Update(float dt) {
-    if(Player::player) {
+    if (Player::player) {
         auto playerBox = Player::player->GetAssociatedBox();
 
         if (state == E_STOPPED) {
+
             if (playerBox.x > associated.box.x - PLAYER_DISTANCE_OFFSET && !fell) {
                 state = E_FALLING;
                 if (!fell) {
@@ -45,42 +48,60 @@ void FallingChasingEnemy::Update(float dt) {
                     associated.AddComponent(new Gravity(associated));
                 }
             }
+
         } else if (state == E_IDLE) {
+
             hit = false;
             idleTimer.Update(dt);
             if (idleTimer.Get() > IDLE_TIME) {
                 state = E_CHASING;
                 idleTimer.Restart();
             }
+
         } else if (state == E_CHASING) {
+
             if (!IsCloseEnoughToPlayer(PLAYER_DISTANCE_THRESHOLD)) {
                 if (associated.box.x > playerBox.x + PLAYER_DISTANCE_THRESHOLD) {
-                    associated.orientation = RIGHT;
+                    associated.orientation = LEFT;
                     associated.box.x -= SPEED * dt;
                 } else if (associated.box.x < playerBox.x - PLAYER_DISTANCE_THRESHOLD + associated.box.w / 2) {
-                    associated.orientation = LEFT;
+                    associated.orientation = RIGHT;
                     associated.box.x += SPEED * dt;
                 }
             } else {
                 state = E_PREPARING;
             }
+
         } else if (state == E_PREPARING) {
+
             preparingTimer.Update(dt);
             if (preparingTimer.Get() > preparing.frameCount * preparing.frameTime) {
                 state = E_ATTACKING;
                 preparingTimer.Restart();
+                auto attackingGO(new GameObject);
+                auto attackingSound(new Sound(*attackingGO, "audio/INIMIGO ATACANDO.ogg"));
+                attackingSound->Play();
+                attackingGO->AddComponent(attackingSound);
+                Game::GetInstance().GetCurrentState().AddObject(attackingGO);
             }
+
         } else if (state == E_ATTACKING) {
+
             attackingTimer.Update(dt);
             if (attackingTimer.Get() > attacking.frameCount * attacking.frameTime) {
                 state = E_IDLE;
                 attackingTimer.Restart();
+            } else if (abs(associated.box.x - playerBox.x) > 25) {
+                associated.box.x += (associated.orientation == RIGHT ? 1 : -1) * SPEED * dt;
             }
+
         } else if (state == E_DEAD_BY_BULLET) {
+
             deadTimer.Update(dt);
             if (deadTimer.Get() > deadByBullet.frameCount * deadByBullet.frameTime) {
                 associated.RequestDelete();
             }
+
         }
 
         if (hp <= 0) {
